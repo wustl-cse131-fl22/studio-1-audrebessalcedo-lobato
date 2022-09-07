795c7942a4c02e1ebf56a3b3e7c7550449a4bdc5
jacquelineau <jacquelineau@jacquelines-mbp.dhcp.wustl.edu>
2022-09-07 15:27:15 -0500

    studio1

--- src/studio1/HiOne.java
+++ src/studio1/HiOne.java
@@ -13,6 +13,7 @@ public class HiOne {
 		System.out.print("Enter value for arg 0: ");
 		String name = in.nextLine();
 		//
+		System.out.println("Hi, " + name + "."+ " How are you?");
 		// Below this line, enter code so that this program's output says
 		//      Hi, Pat.  How are you?
 		// if the value of name is "Pat"
